a6f005a1e1f2ae8a8e0b4d3252c008858e418bb6
GuiFerreiraCode <gfanalyst22@gmail.com>
2024-05-29 21:46:14 -0300

    Projeto DIO Felipão etapa 1

--- src/index.js
+++ src/index.js
@@ -1,2 +1,33 @@
-let test = "apenas testando as mudanças"
-console.log(test)
+const digiteSeuNickName = " Digite seu nickname aqui" ;
+const complementoXpLevel = " pontos de experiência sendo " ;
+let nickName = "Cleitin" ;
+let xpLevel = 6459 ;
+
+console.log(digiteSeuNickName + " = " + nickName) ; 
+
+
+if(xpLevel >= 0 && xpLevel <= 1000 ){
+    console.log(" O " + nickName + "possui " + xpLevel + complementoXpLevel + "ferro " );
+
+}else if(xpLevel >= 1001 && xpLevel <= 2000 ){
+    console.log(" O " + nickName + "possui " + xpLevel + complementoXpLevel + "bronze " );
+
+}else if(xpLevel >= 2001 && xpLevel <= 5000 ){
+    console.log(" O " + nickName + "possui " + xpLevel + complementoXpLevel + "prata " );
+
+}else if(xpLevel >= 5001 && xpLevel <= 7000 ){
+    console.log(" O " + nickName + "possui " + xpLevel + complementoXpLevel + "ouro " );
+
+}else if(xpLevel >= 7.001 && xpLevel <= 8000 ){
+    console.log(" O " + nickName + "possui " + xpLevel + complementoXpLevel + "platina " );
+
+}else if(xpLevel >= 8.001 && xpLevel <= 9000 ){
+    console.log(" O " + nickName + "possui " + xpLevel + complementoXpLevel + "ascendente " );
+
+}else if(xpLevel >= 9.001 && xpLevel <= 10000 ){
+    console.log(" O " + nickName + "possui " + xpLevel + complementoXpLevel + "Imortal ");
+
+}else{
+    console.log(" O " + nickName + " possui " + xpLevel +  complementoXpLevel + "radiante ");
+}
+
